4bf7f1e82a5fb5f8ca266c8e41c73da43917fc6c
Lieutenantz <61732034+Lieutenantz@users.noreply.github.com>
2023-05-28 15:16:51 +0800

    Update clipkits.py

--- clipkits.py
+++ clipkits.py
@@ -7,3 +7,10 @@ def TextToFeature(text):
     with torch.no_grad():
         text_features = model.encode_text(texts)
     return text_features[0]
+
+def ImageToFeature(image):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+    with torch.no_grad():
+        feature = model.encode_image(preprocess(image).unsqueeze(0).to(device)).squeeze(0)
+    return feature
