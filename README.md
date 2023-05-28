d54e900c460a2de6be431c88de8fa9eb4a8e4b83
jcq <xiaomi520yx@163.com>
2023-05-28 14:25:23 +0800

    jcq: 完成了mongodb的初步搭建,结构和参考仓库中的结构一样

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WSMporj
+## jcq
+import_images.py 导入了clip_model库, 但是目前仓库没有clip_model库的实现
+
+config.json只添加了导入数据库的必要配置数据, 并不是参考仓库中的完整config
+
+使用md5命名的图片文件夹data并没有放在仓库中, 因为有3GB, 如果全部放进去, pull 和 push 很麻烦, 如果需要再说
--- import_images.py
+++ import_images.py
@@ -3,28 +3,85 @@ import os
 import shutil
 from glob import glob
 from datetime import datetime
-from PIL import Image
-
+import numpy as np
 import pymongo
 from pymongo.collection import Collection
+
+from tqdm import tqdm
+
+import clip_model
+
 import utils
 
-try:
-    import clip_model
-except ModuleNotFoundError:
-    print("No module named 'clip_model'")
-    clip_tag = False
-else:
-    clip_tag = True
-
-def import_single_image(filepath, feature_str:str, config:dict, mongo_collection, copy=False):
-    filetype = utils.get_file_type(filepath)
-    if filetype is None:
-        print("skip file:", filepath)
+def import_single_image(image_path: str, feature: np.ndarray,
+                        config: dict, mongo_collection: Collection, copy=False):
+    """
+        一张新图片地址, 和由模型生成的feature存入指定的mongo_collection中
+    """
+    image_type = utils.get_file_type(image_path)
+    if image_type is None:
+        print("skip file:", image_path)
         return
 
-    image_size = Image.open(filepath).size()
-    image_feature = utils.str_2_feature_numpy(feature_str)
+    image_size = utils.get_image_size(image_path)
+
+    feature = feature.astype(config['storage-type']) # float32
 
     if copy:
-        md5hash = utils.calc_md5(filename)
+        md5hash = utils.calc_md5(image_path)
+        new_basename = md5hash + '.' + image_type # new name
+        new_full_path = utils.get_full_path(config['import-image-base'], new_basename)
+
+        if os.path.isfile(new_full_path):
+            print("duplicate file:", image_path)
+            return
+        
+        shutil.copy2(image_path, new_full_path)
+        stat = os.stat(new_full_path)
+    else:
+        stat = os.stat(image_path)
+        new_full_path = image_path
+
+    image_mtime = datetime.fromtimestamp(stat.st_mtime)
+    image_datestr = image_mtime.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+    # save to mongodb
+    document = {
+        'filename': new_full_path, # md5 name
+        'extension': image_type,
+        'height': image_size[1],
+        'width': image_size[0],
+        'filesize': stat.st_size,
+        'date': image_datestr,
+        'feature': feature.tobytes() # feature 存储的是byte格式
+    }
+
+    x = mongo_collection.insert_one(document)
+    return x
+
+def import_dir(base_dir: str, model: clip_model.CLIPModel,
+               config: dict, mongo_collection: Collection, copy=False):
+    """
+        从一个存放着多张图片的文件夹中导入所有图片到数据库中
+    """
+    filelist = glob(os.path.join(base_dir, '**/*'), recursive=True) # base_dir下的所有文件和文件夹列表
+    filelist = [f for f in filelist if os.path.isfile(f)]
+
+    for filename in tqdm(filelist):
+        feature = model.get_image_feature(filename)
+        import_single_image(filename, feature, config, mongo_collection, copy=copy)
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--copy', action='store_true')
+    parser.add_argument('dir')
+    args = parser.parse_args()
+
+    config = utils.get_config()
+    mongo_collection = utils.get_mongo_collection()
+    model = clip_model.get_model()
+    import_dir(args.dir, model, config, mongo_collection, args.copy)
+
+if __name__ == '__main__':
+    main()
