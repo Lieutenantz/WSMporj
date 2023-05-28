a6f44a7dd79b0f93574bd1731df0cb8e915d4ffa
Lieutenantz <61732034+Lieutenantz@users.noreply.github.com>
2023-05-28 15:25:33 +0800

    Update clipkits.py
    调用ClipTool来使用函数

--- clipkits.py
+++ clipkits.py
@@ -1,16 +1,39 @@
 import torch
 import clip
-def TextToFeature(text):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
-    texts = clip.tokenize([text]).to(device)
-    with torch.no_grad():
-        text_features = model.encode_text(texts)
-    return text_features[0]
+# def TextToFeature(text):
+#     device = "cuda" if torch.cuda.is_available() else "cpu"
+#     model, preprocess = clip.load("ViT-B/32", device=device)
+#     texts = clip.tokenize([text]).to(device)
+#     with torch.no_grad():
+#         text_features = model.encode_text(texts)
+#     return text_features[0]
 
-def ImageToFeature(image):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
-    with torch.no_grad():
-        feature = model.encode_image(preprocess(image).unsqueeze(0).to(device)).squeeze(0)
-    return feature
+# def ImageToFeature(image):
+#     device = "cuda" if torch.cuda.is_available() else "cpu"
+#     model, preprocess = clip.load("ViT-B/32", device=device)
+#     with torch.no_grad():
+#         feature = model.encode_image(preprocess(image).unsqueeze(0).to(device)).squeeze(0)
+#     return feature
+class ClipTool():
+    def __init__(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model, preprocess = clip.load("models/ViT-B-32.pt", device=device)
+        self.device=device
+        self.model=model
+        self.preprocess=preprocess
+
+    def TextToFeature(self,text):
+        model=self.model
+        device=self.device
+        texts = clip.tokenize([text]).to(device)
+        with torch.no_grad():
+            text_features = model.encode_text(texts)
+        return text_features[0]
+
+    def ImageToFeature(self,image):
+        model = self.model
+        device = self.device
+        preprocess=self.preprocess
+        with torch.no_grad():
+            feature = model.encode_image(preprocess(image).unsqueeze(0).to(device)).squeeze(0)
+        return feature
